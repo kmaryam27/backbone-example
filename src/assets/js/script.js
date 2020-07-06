41ae488d4ec1a29eff86924d0422e8b8358f1750
k_maryam27 <k_maryam27@yahoo.com>
2020-07-06 10:04:57 -0400

    backbone functions

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -24,6 +24,40 @@ const BBV1 = Backbone.View.extend({
     initialize: function(){
         this.template = _.template($('.models-list-templete').html())
     },
+    events:{
+        'click .edit-model': 'edit',
+        'click .edit-update': 'update',
+        'click .edit-cancel': 'cancel',
+        'click .delete-model': 'delete'
+    },
+    edit: function(){
+        const name = this.$('.name').html();
+        const family = this.$('.family').html();
+        const email = this.$('.email').html();
+        const phone = this.$('.phone').html();
+
+        this.$('.name').html(`<input type='text' class='form-control name-update' value='${name}'/>`)
+        this.$('.family').html(`<input type='text' class='form-control family-update' value='${family}'/>`)
+        this.$('.email').html(`<input type='text' class='form-control email-update' value='${email}'/>`)
+        this.$('.phone').html(`<input type='text' class='form-control phone-update' value='${phone}'/>`)
+        
+        $('.edit-model').hide();
+        $('.delete-model').hide();
+        this.$('.edit-update').show();
+        this.$('.edit-cancel').show();
+    },
+    update: function(){//in the using arrow function this does not refer to parent block
+        this.model.set('name', $('.name-update').val());
+        this.model.set('family', $('.family-update').val());
+        this.model.set('email', $('.email-update').val());
+        this.model.set('phone', $('.phone-update').val());
+    },
+    cancel: () => {
+        modelsView.render();//modelsView = new BBCV()
+    },
+    delete: function(){
+        this.model.destroy()
+    },
     render: function(){
         this.$el.html(this.template(this.model.toJSON()));
         return this;
@@ -35,7 +69,17 @@ const BBVC = Backbone.View.extend({
     model: c1,
     el: $('.models-list'),
     initialize: function(){
-        this.model.on('add', this.render, this)
+        const self = this;
+        this.model.on('add', this.render, this);
+        // this.model.on('change', this.render, this)//in this method because we need time just will update first culumn's update
+        this.model.on('change', function(){
+            setTimeout(() => {//we need the timer to wait for all -update values to get and change
+                self.render();
+            }, 30)
+            
+        }, this);
+
+        this.model.on('remove', this.render, this);
     },
     render: function(){
         this.$el.html('');
